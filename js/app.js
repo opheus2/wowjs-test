5d893d5abda05ad9d6f802e0ebcf2abb8f95ab59
opheus2 <j.ominiabohs@gmail.com>
2020-08-08 05:00:59 +0200

    add

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+
+
+window.addEventListener('load', myFunction)
+
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("content").style.display = "block";
+}
